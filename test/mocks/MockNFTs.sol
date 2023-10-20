e5dd72447e98310c57aa079f9173c0e8116154fb
Hangleang <hangleang127@gmail.com>
2023-10-20 16:50:57 +0700

    refactor: refactor strategy contract and test case

--- test/mocks/MockNFTs.sol
+++ test/mocks/MockNFTs.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 import "charii-contract/ChariiNFTs.sol";
+
 contract MockNFTs is ChariiNFTs {
-    constructor(address owner) ChariiNFTs("ChariiNFTs", "CHARII", owner, owner, 500) {}
+    // 5% in default royalty, 1% in platform fee
+    constructor(address owner) ChariiNFTs("ChariiNFTs", "CHARII", owner, owner, owner, 500, 100) {}
 }
